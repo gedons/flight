5f33e716b8c4e06d1962596622ea32c0a8308325
Nerdy Gedoni <gedoniani@gmail.com>
2024-05-23 09:54:45 +0100

    search criteria

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -25,4 +25,40 @@ const getFlightById = async (req, res) => {
   }
 };
 
-module.exports = { getAllFlights, getFlightById };
+// Search, filter, and sort flights
+const searchFlights = async (req, res) => {
+  try {
+    const { departure, destination, departureDate, returnDate, sortBy, order } = req.query;
+
+    let query = {};
+
+    if (departure) {
+      query.departure = { $regex: departure, $options: 'i' }; // Case-insensitive match
+    }
+
+    if (destination) {
+      query.destination = { $regex: destination, $options: 'i' }; // Case-insensitive match
+    }
+
+    if (departureDate) {
+      query.departureTime = { $gte: new Date(departureDate) };
+    }
+
+    if (returnDate) {
+      query.arrivalTime = { $lte: new Date(returnDate) };
+    }
+
+    let sortCriteria = {};
+    if (sortBy) {
+      sortCriteria[sortBy] = order === 'desc' ? -1 : 1;
+    }
+
+    const flights = await Flight.find(query).populate('category').sort(sortCriteria);
+
+    res.json(flights);
+  } catch (error) {
+    res.status(500).json({ message: 'Error searching flights' });
+  }
+};
+
+module.exports = { getAllFlights, getFlightById, searchFlights };
--- routes/flightRoutes.js
+++ routes/flightRoutes.js
@@ -1,10 +1,11 @@
 // backend/routes/flightRoutes.js
 const express = require('express');
-const { getAllFlights, getFlightById } = require('../controllers/flightController');
+const { getAllFlights, getFlightById, searchFlights  } = require('../controllers/flightController');
 
 const router = express.Router();
 
 router.route('/').get(getAllFlights);
+router.route('/search').get(searchFlights); 
 router.route('/:id').get(getFlightById);
 
 module.exports = router;
